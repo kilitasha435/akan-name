4fe6392a359a34e3d83375fb8979ee070d4d9168
kevin-kili <kilikevin435@gmail.com>
2020-02-13 17:49:24 +0300

    added navs

---
+++ script.js
@@ -0,0 +1,5 @@
+function validation(){
+    var dateOfBirth = document.getElementById("date of birth");
+    var monthOfBirth = document.getElementById("month of birth");
+    var yearOfBirth = document.getElementById("year of birth") 
+}
