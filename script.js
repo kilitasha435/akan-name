22fbda9af5f27a44b6d9ec1ecc088c5b277d9fd2
kevin-kili <kilikevin435@gmail.com>
2020-02-14 11:33:54 +0300

    added image

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+var maleNames = ["Kwasi","Kwandwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var femaleNames = ["Akosua","Adwoa","Abenea","Akua","Yaa","Afua","Ama"]
+var monthOfBirth = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 function validation(){
-    var dateOfBirth = document.getElementById("date of birth");
+    var dateOfBirth = document.getElementById("date of birth").innerHTML.valueOfBirth;
     var monthOfBirth = document.getElementById("month of birth");
-    var yearOfBirth = document.getElementById("year of birth") 
+    var yearOfBirth = document.getElementById("year of") 
 }
