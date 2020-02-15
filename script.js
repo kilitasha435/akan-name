f4b297095d1faabff287d570b3c1989f4db96800
kevin-kili <kilikevin435@gmail.com>
2020-02-15 10:02:03 +0300

    added image

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function getName(){
     
 
     if(day<=0||day>31)
-    alert("Oops!please enter a valid day");
+    alert("Please enter a valid day");
     else if(month<0||month>12 ||month ==2 && day>29)
-    alert("Oops!please enter a valid month");
+    alert("Please enter a valid month");
     var day = new Date(year + "/" + month + "/" + day);
     var birthDay=day.getDay();
     if(male.checked==true){
